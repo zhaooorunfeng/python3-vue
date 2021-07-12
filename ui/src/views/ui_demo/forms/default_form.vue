3e830a43482dae762424bae13d8429dd33cbd9c2
daominaru <1433149685@qq.com>
2021-07-12 15:47:18 +0800

    minor: 样式调整

--- ui/src/views/ui_demo/forms/default_form.vue
+++ ui/src/views/ui_demo/forms/default_form.vue
@@ -214,7 +214,6 @@
             .footer {
                 position: fixed;
                 width: 100%;
-                background-color: #fff;
                 bottom: 0px;
                 height: 52px;
                 line-height: 52px;
