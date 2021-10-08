ffff96b54c3a2cc19859d50684298f55e405dadf
juneyan <juneyan@canway.net>
2021-10-08 16:05:28 +0800

    optimization: 解构默认值

--- ui/src/api/config/index.js
+++ ui/src/api/config/index.js
@@ -34,7 +34,7 @@ service.interceptors.request.use(config => {
 // response interceptor
 service.interceptors.response.use(
     response => {
-        const { status, data } = response
+        const { status, data } = response || {}
         if (status !== 200) {
             return {
                 result: false,
