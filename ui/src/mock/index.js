843af484dc1d8ac6a4e0d9997446d2591e921555
juneyan <juneyan@canway.net>
2021-10-12 13:56:19 +0800

    optimization: 优化Mock模块的代码

--- ui/src/mock/index.js
+++ ui/src/mock/index.js
@@ -1,4 +1,16 @@
 import test from './test.js'
+
+/**
+ * @description 格式化Mock接口返回的数据
+ * @param {{data:any,result:boolean,messge:string}}
+ * @returns {{data:any,result:boolean,messge:string}}
+ */
+export const formatResult = ({ data, result = true, messge = 'woops!' }) => ({
+    result,
+    data,
+    messge
+})
+
 /**
  * 模拟数据mock
  *
